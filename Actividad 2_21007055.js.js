1f2697af0522fcd196bc6834a5308ed925f65f38
KevinCordonEDU <78771195+KevinCordonEDU@users.noreply.github.com>
2021-02-08 21:09:25 -0600

    Add files via upload

---
+++ Actividad 2_21007055.js.js
@@ -0,0 +1,23 @@
+var mes = prompt ("Cual es el numero del mes?");
+
+if ( mes == 12|| mes == 1 || mes == 2 ) {
+ alert("Es invierno");
+
+ }
+
+else if (mes == 3 || mes == 4 || mes == 5){
+   alert ("Es primavera");
+ }
+
+else if (mes == 6 || mes == 7 || mes == 8){
+  alert ("Es Verano");
+}
+
+else if (mes == 9 || mes == 10 || mes == 11){
+  alert ("Es Otoño");
+}
+else {
+  alert ("Ingrese un numero entre 1 y 12")
+}
+
+alert ("Carnet: 21007055" + "  " + "Nombre: Kevin Manolo Cordon Rivera");
